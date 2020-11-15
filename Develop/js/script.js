847b827eeca5a4a877a74893363f0a21c70282d1
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2020-11-15 13:26:45 -0500

    update readme

---
+++ Develop/js/script.js
@@ -0,0 +1,72 @@
+var lead = moment().format("dddd, MMMM Do YYYY, h:mm a"); 
+
+// Current day and time display at the top of the page
+$("#currentDay").html(lead);
+
+console.log(lead)
+
+
+//Saving  
+$(document).ready(function () {
+
+    $(".saveBtn").on("click", function (){
+
+       var text =  $(this).siblings(".description").val();
+       var time =  $(this).parent().attr("id");
+
+        
+
+        //Save in local storage
+        localStorage.setItem(time, text);
+
+    })
+
+    function timeTrack() {
+        var currentTime = moment().hour();
+
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+
+            if (blockTime < currentTime) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+
+
+            }else if (blockTime === currentTime) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+
+            }else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+
+                
+            }
+        })
+    }
+
+    
+  
+   
+
+    //Local Storage
+    $("#time8 .description").val(localStorage.getItem("time8"));
+    $("#time9 .description").val(localStorage.getItem("time9"));
+    $("#time10 .description").val(localStorage.getItem("time10"));
+    $("#time11 .description").val(localStorage.getItem("time11"));
+    $("#time12 .description").val(localStorage.getItem("time12"));
+    $("#time13 .description").val(localStorage.getItem("time13"));
+    $("#time14 .description").val(localStorage.getItem("time14"));
+    $("#time15 .description").val(localStorage.getItem("time15"));
+    $("#time16 .description").val(localStorage.getItem("time16"));
+    $("#time17 .description").val(localStorage.getItem("time17"));
+
+
+
+    timeTrack();
+    
+})
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Work Day Scheduler Starter Code
+Work Day Scheduler Starter Code
+
+
+# Linked
+
+[Github](https://github.com/wdavonta/workday-scheduler.git "Work Day Scheduler Repo"
+
+[Link] ( https://wdavonta.github.io/workday-scheduler/)
+
+
+# About the project
+This project is about  using a daily planner to create a schedule. The current day is displayed at the top of the calendar. While scrolling presented with time blocks for standard business hours. Each time block is color-coded to indicate whether it is in the past, present, or future.
+You be able to click on an event and click the save button for that time block. The text for that event is saved in local storage everytime the page is refresh the saved events persist.
+
+
+![alt text](png "Screenshot of webite")
+
+# Contact Info
+Da'Vonta Williamson
+
+E: <a href="mailto:wdavonta@gmail.com">wdavonta@gmail.com</a>
+                </address>
+
